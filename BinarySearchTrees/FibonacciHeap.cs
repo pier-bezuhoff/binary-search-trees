37ee694dc35f7d795392e7b1303560487d1e71fe
Pierre Bezuhoff <pierbezuhoff2016@gmail.com>
2018-11-06 14:29:17 +0300

    added FibonacciHeap

---
+++ BinarySearchTrees/FibonacciHeap.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTrees
+{
+    class FibonacciHeap<E> where E: IComparable<E>
+    {
+        public FibonacciHeapNode<E> root = null;
+        int nNodes = 0;
+
+        public FibonacciHeap() {}
+
+        IEnumerable<FibonacciHeapNode<E>> Roots => root.Siblings;
+
+        public void Include(E key)
+        {
+            Include(new FibonacciHeapNode<E>(key));
+        }
+
+        public void Include(FibonacciHeapNode<E> node)
+        {
+            if (root == null)
+                root = node;
+            else
+                root = root.AddLeft(node);
+            nNodes++;
+        }
+
+        public void Merge(FibonacciHeap<E> heap)
+        {
+            if (root == null)
+                root = heap.root;
+            else
+                root = root.AddLeft(heap.root);
+            nNodes += heap.nNodes;
+        }
+
+        public FibonacciHeapNode<E> PopMin()
+        {
+            var oldRoot = root;
+            if (root == null)
+                throw new Exception("empty heap has no min");
+            foreach (var child in root.Children)
+                child.parent = null;
+            var newRoot = root.Remove();
+            nNodes--;
+            if (newRoot == null) // single root
+                root = root.child;
+            else
+            {
+                root = newRoot;
+                Consolidate();
+            }
+            return oldRoot;
+        }
+
+        void DecreaseKey(FibonacciHeapNode<E> node, E newKey)
+        {
+            if (node.key.CompareTo(newKey) < 0)
+                throw new Exception($"new key {newKey} > old key {node.key} of {node}");
+            else if (node.key.CompareTo(newKey) > 0)
+            {
+                node.key = newKey;
+                var y = node.parent;
+                if (y != null && node.CompareTo(y) < 0)
+                {
+                    // cut node y
+                    // cascading cut y
+                }
+                if (node.CompareTo(root) < 0)
+                    root = node;
+            }
+
+        }
+
+        void Consolidate()
+        {
+            // TODO: understand Consolidate, Cut and CascadingCut
+            // A[i] = y <=> y.nChildren = i && y <- Roots
+            List<FibonacciHeapNode<E>> A = Enumerable.Repeat<FibonacciHeapNode<E>>(
+                null,
+                1 + Roots.Select(r => r.nChildren).Max()).ToList();
+            foreach(var node in Roots)
+            {
+                var x = node;
+                int d = node.nChildren;
+                while (A[d] != null)
+                {
+                    var y = A[d];
+                    if (x.CompareTo(y) > 0)
+                    {
+                        var tmp = x;
+                        x = y;
+                        y = tmp;
+                    }
+                    y.Remove();
+                    x.AddChild(y);
+                    y.marked = false;
+                    A[d] = null;
+                    d++;
+                }
+                A[d] = x;
+            }
+            root = null;
+            foreach (var a in A.Where(x => x != null))
+                Include(a);
+        }
+
+        void Cut(FibonacciHeapNode<E> node, FibonacciHeapNode<E> parent)
+        {
+            parent.RemoveChild(node);
+            Include(node);
+            node.marked = false;
+        }
+
+        void CascadingCut(FibonacciHeapNode<E> node)
+        {
+            var parent = node.parent;
+            if (parent != null)
+                if (!node.marked)
+                    node.marked = true;
+                else
+                {
+                    Cut(node, parent);
+                    CascadingCut(parent);
+                }
+        }
+    }
+}
---
+++ BinarySearchTrees/FibonacciHeapNode.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTrees
+{
+    class FibonacciHeapNode<E>: IComparable<FibonacciHeapNode<E>> where E : IComparable<E>
+    {
+        public E key;
+        public FibonacciHeapNode<E> parent = null;
+        public FibonacciHeapNode<E> child = null;
+        public FibonacciHeapNode<E> left, right;
+        public int nSiblings = 1;
+        public int nChildren = 0;
+        public bool marked = false;
+
+        public FibonacciHeapNode(E key) {
+            this.key = key;
+            left = right = this;
+        }
+
+        override public string ToString() => ToString(0);
+
+        private string ToString(int depth)
+        {
+            string tab = new string(' ', 4 * depth);
+            return string.Join("\n", Siblings.Select(
+                node => $"{tab}(Node {node.key}\n{node.child.ToString(depth + 1)})"));
+        }
+
+        public IEnumerable<FibonacciHeapNode<E>> Siblings
+        {
+            get
+            {
+                yield return this;
+                var sibling = left;
+                while (sibling != this)
+                {
+                    yield return sibling;
+                    sibling = sibling.left;
+                }
+            }
+        }
+
+        public IEnumerable<FibonacciHeapNode<E>> Children =>
+            child == null? Enumerable.Empty<FibonacciHeapNode<E>>() : child.Siblings;
+
+        public int CompareTo(FibonacciHeapNode<E> node)
+        {
+            return key.CompareTo(node.key);
+        }
+
+        public FibonacciHeapNode<E> Remove()
+        {
+            if (nSiblings == 1)
+                return null;
+            left.right = right;
+            right.left = left;
+            return left.Siblings.Min();
+        }
+
+        public FibonacciHeapNode<E> AddLeft(FibonacciHeapNode<E> node)
+        {
+            node.left.left = left;
+            node.right = this;
+            left.right = node.left;
+            left = node;
+            // and merge similar trees
+            nSiblings += node.nSiblings;
+            if (this.CompareTo(node) < 0)
+                return this;
+            else
+                return node;
+        }
+
+        public FibonacciHeapNode<E> AddRight(FibonacciHeapNode<E> node)
+        {
+            node.right.right = right;
+            node.left = this;
+            right.left = node.right;
+            right = node;
+            // and merge similar trees
+            nSiblings += node.nSiblings;
+            if (this.CompareTo(node) < 0)
+                return this;
+            else
+                return node;
+        }
+
+        public void RemoveChild(FibonacciHeapNode<E> node)
+        {
+            if (node == child)
+                child = node.Remove();
+            else
+                node.Remove();
+            node.parent = null;
+            nChildren--;
+        }
+
+        public void AddChild(FibonacciHeapNode<E> node)
+        {
+            if (child == null)
+                child = node;
+            else
+                child = child.AddLeft(node);
+            node.parent = this;
+            nChildren++;
+        }
+
+        /* merge two trees if they share the same form */
+        public FibonacciHeapNode<E> Merge(FibonacciHeapNode<E> first, FibonacciHeapNode<E> second)
+        {
+            if (first.CompareTo(second) > 0)
+            {
+                second.AddChild(first);
+                return second;
+            }
+            else // first.key != second.key!
+            {
+                first.AddChild(second);
+                return first;
+            }
+        }
+
+        void MergeSimilar(FibonacciHeapNode<E> node)
+        {
+            if (node.nSiblings > 1)
+            {
+                //
+            }
+        }
+    }
+}
